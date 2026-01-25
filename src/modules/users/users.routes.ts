e2bd9499da352a31ba20ccbcb3430ea84a8eeae7
Vladimir Azpeitia <vazpeitiah@gmail.com>
2026-01-24 19:27:14 -0600

    refactor: reorder user registration endpoint for clarity in route definitions

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -2,10 +2,7 @@ import type { FastifyInstance } from 'fastify'
 import { getUserListController, createUserController } from './users.controller'
 
 export default function (app: FastifyInstance) {
-  // Endpoint público para registro de usuarios
-  app.post('/register', createUserController)
-
-  // Endpoints protegidos
   app.addHook('preHandler', app.authenticate)
   app.get('/list', getUserListController)
+  app.post('/register', createUserController)
 }
