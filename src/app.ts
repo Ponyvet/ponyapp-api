39bdaa8cf2cf50b749522bbfc0fa46aa5e43365c
Vladimir Azpeitia <vazpeitiah@gmail.com>
2026-02-18 22:29:06 -0600

    fix(cors): add production origin for CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ export const buildApp = () => {
   const app = Fastify({ logger: true })
 
   app.register(cors, {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://app.ponyvet.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
